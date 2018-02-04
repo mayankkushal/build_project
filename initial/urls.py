21e01e2ebaee0187ea4aacb9ff8550f74aa8796a
Mayank Kushal <mayankkushal26@gmail.com>
2018-02-04 23:24:41 +0530

    Add submit of form

--- initial/urls.py
+++ initial/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^', views.BasicView.as_view(), name='index')
+	url(r'^', views.form, name='index')
 ]
--- initial/views.py
+++ initial/views.py
@@ -9,16 +9,26 @@ from .forms import BasicForm
 
 # Create your views here.
 
-class BasicView(FormView):
-	form_class = BasicForm
-	template_name = "initial/form.html"
-	success_url = "/"
+# class BasicView(FormView):
+# 	form_class = BasicForm
+# 	template_name = "initial/form.html"
+# 	success_url = "/"
 
-	def form_valid(self, form):
-		form.save()
-		return super(BasicView, self).form_valid(form)
+# 	def form_valid(self, form):
+# 		print(self.request.GET)
+# 		return super(BasicView, self).form_valid(form)
 
-	def get_context_data(self, **kwargs):
-		context = super(BasicView, self).get_context_data(**kwargs)
-		# context['push'] = BasicModel.objects.all()
-		return context
+# 	def get_context_data(self, **kwargs):
+# 		context = super(BasicView, self).get_context_data(**kwargs)
+# 		# context['push'] = BasicModel.objects.all()
+# 		return context
+
+
+def form(request):
+	if request.method == 'POST':
+		for p, k in request.POST.items():
+			print(p, k)
+		return render(request, "initial/form.html")
+
+	if request.method == "GET":
+		return render(request, "initial/form.html")
